3f0e41822d0fa752dd65980c46c4e374bf38aca1
Alejandro Bustamante <a.bustamante@erunner.eu>
2015-02-16 15:36:37 +0100

    parameterized GET /reservations endpoint

--- src/Gtb/Bundle/ApiBundle/Controller/ReservationController.php
+++ src/Gtb/Bundle/ApiBundle/Controller/ReservationController.php
@@ -6,12 +6,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Util\Codes as ResponseCodes;
-use Gtb\Bundle\ApiBundle\Exception\PersonNotAvailableException;
-use Gtb\Bundle\ApiBundle\Exception\RestaurantNotAvailableException;
 use Gtb\Bundle\CoreBundle\Entity\Reservation;
 use Gtb\Bundle\CoreBundle\Form\ReservationType;
 use Symfony\Component\HttpFoundation\Request;
-use Gtb\Bundle\CoreBundle\DateReservation\DateReservationUtils;
 
 class ReservationController extends FOSRestController
 {
@@ -26,12 +23,40 @@ class ReservationController extends FOSRestController
      */
     public function getReservationsAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
+        $qb = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('GtbCoreBundle:Reservation')
+            ->createQueryBuilder('r')
+        ;
 
-        $entities = $em->getRepository('GtbCoreBundle:Reservation')->findAll();
+        if ($request->query->get('y') &&
+            $request->query->get('m') &&
+            $request->query->get('d')) {
+            $date = new \DateTime(sprintf('%d-%d-%d',
+                $request->query->get('y'),
+                $request->query->get('m'),
+                $request->query->get('d')
+            ));
+            $qb->andWhere($qb->expr()->between('r.date', ':date_from', ':date_to'));
+            $qb->setParameter('date_from', $date, \Doctrine\DBAL\Types\Type::DATETIME);
+            $qb->setParameter('date_to', $date, \Doctrine\DBAL\Types\Type::DATETIME);
+
+            // strict date comparison works when there isn't a date range being queried for
+            // left here for reference (as no commented code should reach production!)
+            //$qb->andWhere('r.date = :date')->setParameter('date', $date, \Doctrine\DBAL\Types\Type::DATETIME);
+        }
+
+        if ($restaurantId = $request->query->get('rid')) {
+            $qb->andWhere('r.restaurant = :rid')->setParameter('rid', $restaurantId);
+        }
+
+        if ($personId = $request->query->get('pid')) {
+            $qb->andWhere('r.person = :pid')->setParameter('pid', $personId);
+        }
 
         return array(
-            'entities' => $entities,
+            'entities' => $qb->getQuery()->getResult(),
         );
     }
 
